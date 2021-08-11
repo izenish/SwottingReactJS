bd7f52c6621bea7f6518840f754add606b58bbcc
Zenish <zenish77@gmail.com>
2021-08-11 09:18:41 +0545

    build complete

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const About = () => {
+  return (
+    <div>
+      <h5>Version 2</h5>
+      <Link to="/">
+        <h4>Go back</h4>
+      </Link>
+    </div>
+  );
+};
+
+export default About;
