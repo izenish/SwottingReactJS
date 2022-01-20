ce7c7de97e6213ee3774807378f31bb73f80d14e
Zenish <zenish77@gmail.com>
2022-01-20 19:17:52 +0545

    Fresh start

---
+++ project1/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="title">This is my first project</h1>
+    </div>
+  );
+}
